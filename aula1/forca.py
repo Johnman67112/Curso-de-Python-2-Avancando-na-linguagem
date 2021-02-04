eb03b25b8ab1c2efa84ac57c2a02df2ebbd9aa14
João <joaovitorcucomo@gmail.com>
2021-02-04 10:37:14 -0300

    Aula 1: Preparando a forca e usando booleano

---
+++ aula1/forca.py
@@ -0,0 +1,17 @@
+def jogar():
+    print("*********************************")
+    print("***Bem vindo ao jogo da Forca!***")
+    print("*********************************")
+
+    palavra_secreta = "banana"
+    print(palavra_secreta)
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+        print("Jogando...")
+    print("Fim do jogo")
+
+if(__name__ == "__main__"):
+    jogar()
